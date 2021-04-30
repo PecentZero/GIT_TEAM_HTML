83c5e4b9b36db649f9a59b6051ea80c06a274b4b
PecentZ <security428@naver.com>
2021-05-01 07:59:42 +0900

    debugging register.cpp

--- html/cgi/cgi-bin/register.cpp
+++ html/cgi/cgi-bin/register.cpp
@@ -14,19 +14,12 @@ using namespace cgicc;
 // registration parameter 
 // Username , Email , Password , ConfirmPassword
 
-void Check_Element(form_iterator &f,string name) // print parameter's value
-{ //also needed check space or valid value
-	if(!f->isEmpty() && f != (*formData).end()){
-	cout<"<<p>"<< name <<":" <<**f<<"</p>"<<endl;
-	}else{
-	cout <<"NO" <<name <<"entered" <<endl;
 
-}
-
+Cgicc formData;
+void Check_Element(form_iterator &f,const std::string name); // print parameter's value
 
 int main()
-{
-	Cgicc formData;
+{	
    	cout << "<html>\n";
    	cout << "<head>\n";
   	cout << "<title>Text Area Data to CGI</title>\n";
@@ -38,14 +31,23 @@ int main()
 	form_iterator f_Password;
 	form_iterator f_ConfirmPassword;
 
-	Check_Element(f_Username,string("Username");
-	Check_Element(f_Username,string("Email");
-	Check_Element(f_Username,string("Password");
-	Check_Element(f_Username,string("ConfirmPassword");
+	Check_Element(f_Username,string("Username"));
+	Check_Element(f_Username,string("Email"));
+	Check_Element(f_Username,string("Password"));
+	Check_Element(f_Username,string("ConfirmPassword"));
 	
 	cout << "<br>\n";
 	cout <<"</body>\n";
 	cout <<"</html>\n";
 
 	return 0;
+}
+
+void Check_Element(form_iterator &f,const std::string name) // print parameter's value
+{ //also needed check space or valid value
+	if(!f->isEmpty() && f != (*formData).end()){
+	cout<<"<<p>"<< name <<":" <<**f<<"</p>"<<endl;
+	}else{
+	cout <<"NO" <<name <<"entered" <<endl;
+	}
 }
