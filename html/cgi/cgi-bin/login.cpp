39842229b1a61c0f00ee58bf67923d69d4a522a1
PecentZ <security428@naver.com>
2021-05-01 06:28:11 +0900

    testing for receiving data

---
+++ html/cgi/cgi-bin/login.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>  
+#include <string>  
+#include <stdio.h>  
+#include <stdlib.h> 
+
+#include <cgicc/CgiDefs.h> 
+#include <cgicc/Cgicc.h> 
+#include <cgicc/HTTPHTMLHeader.h> 
+#include <cgicc/HTMLClasses.h> 
+
+using namespace std;
+using namespace cgicc;
+
+int main () {
+   Cgicc formData;
+  
+   cout << "Content-type:text/html\r\n\r\n";
+   cout << "<html>\n";
+   cout << "<head>\n";
+   cout << "<title>Testing login</title>\n";
+   cout << "</head>\n";
+   cout << "<body>\n";
+
+   form_iterator f_id = formData.getElement("id");  
+   if( !f_id->isEmpty() && f_id != (*formData).end()) {  
+      cout << "id : " << **f_id << endl;  
+   } else {
+      cout << "none id input" << endl;  
+   }
+   cout << "<br/>\n";
+   cout << "</body>\n";
+   cout << "</html>\n";
+   
+   return 0;
+}
