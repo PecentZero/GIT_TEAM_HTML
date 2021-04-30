aac84e056a87e59fa9e3107c5f82f87b59ff65a5
PecentZ <security428@naver.com>
2021-05-01 08:52:34 +0900

    RE work well

--- html/cgi/cgi-bin/RE.cpp
+++ html/cgi/cgi-bin/RE.cpp
@@ -11,39 +11,39 @@
 
 using namespace std;
 using namespace cgicc;
-// registration parameter
-// Username , Email , Password , ConfirmPassword
 
+int main () {
+  Cgicc formData;
 
-Cgicc formData;
-void Check_Element(form_iterator &f,string name); // print parameter's value
-
-int main()
-{
-    cout << "Content-type:text/html\r\n\r\n";
-   	cout << "<html>\n";
-   	cout << "<head>\n";
-  	cout << "<title>Testing Registration/title>\n";
- 	  cout << "</head>\n";
-  	cout << "<body>\n";
+  cout << "Content-type:text/html\r\n\r\n";
+  cout << "<html>\n";
+  cout << "<head>\n";
+  cout << "<title>Testing login</title>\n";
+  cout << "</head>\n";
+  cout << "<body>\n";
 
 	form_iterator f_Username = formData.getElement("Username"); // handling parameter getting by post method
-	form_iterator f_Email = formData.getElement("Email");
-	form_iterator f_Password = formData.getElement("Password");
-	form_iterator f_ConfirmPassword = formData.getElement("ConfirmPassword");
+  if(!f_Username->isEmpty() && f_Username != (*formData).end()){
+  cout<<"<p>"<< "Username" <<":" <<**f_Username<<"</p>"<<endl;
+  }else{
+  cout <<"<p> NO "<<"Username" <<" entered</p>" <<endl;
+  }
+//	form_iterator f_Email = formData.getElement("Email");
+//	form_iterator f_Password = formData.getElement("Password");
+	//form_iterator f_ConfirmPassword = formData.getElement("ConfirmPassword");
 
-	Check_Element(f_Username,string("Username"));
+/*	Check_Element(f_Username,string("Username"));
 	Check_Element(f_Email,string("Email"));
 	Check_Element(f_Password,string("Password"));
 	Check_Element(f_ConfirmPassword,string("ConfirmPassword"));
-
+*/
 	cout << "<br>\n";
 	cout <<"</body>\n";
 	cout <<"</html>\n";
 	return 0;
 }
 
-void Check_Element(form_iterator &f,string name) // print parameter's value
+/*void Check_Element(form_iterator &f,string name) // print parameter's value
 { //also needed check space or valid value
 	if(!f->isEmpty() && f != (*formData).end()){
 	cout<<"<p>"<< name <<":" <<**f<<"</p>"<<endl;
@@ -51,3 +51,4 @@ void Check_Element(form_iterator &f,string name) // print parameter's value
 	cout <<"<p> NO "<<name <<" entered</p>" <<endl;
 	}
 }
+*/
