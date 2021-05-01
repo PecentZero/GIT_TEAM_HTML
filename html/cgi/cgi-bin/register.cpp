47a6e1734d16b7b626a6ce4285ad6e125afe9b12
PecentZ <security428@naver.com>
2021-05-01 18:11:41 +0900

    connection with mysql

--- html/cgi/cgi-bin/register.cpp
+++ html/cgi/cgi-bin/register.cpp
@@ -4,10 +4,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <cgicc/CgiDefs.h>
+#include <cgicc/CgiDefs.h>  //for cgic programming
 #include <cgicc/Cgicc.h>
 #include <cgicc/HTTPHTMLHeader.h>
 #include <cgicc/HTMLClasses.h>
+
+#include "mysql_connection.h"
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+#include <cppconn/prepared_statement.h>
 
 using namespace std;
 using namespace cgicc;
@@ -17,10 +24,13 @@ using namespace cgicc;
 
 Cgicc formData;
 void Check_Element(form_iterator &f,string name); // print parameter's value
-void trim_space(string &temp) // trim  front , back  space
+string  trim_space(char*);// trim  front , back  space
 
 int main()
 {
+   string username,password,email;
+   int option ; // option = 1  means success option = 0 means fail
+
     cout << "Content-type:text/html\r\n\r\n";
    	cout << "<html>\n";
    	cout << "<head>\n";
@@ -33,15 +43,51 @@ int main()
 	form_iterator f_Password = formData.getElement("Password");
 	form_iterator f_ConfirmPassword = formData.getElement("ConfirmPassword");
 
-  trim_space(f_Username);
-  trim_space(f_Email);
-  trim_space(f_Password);
-  trim_space(f_ConfirmPassword);
+	if(**f_Password != **f_ConfirmPassword) //alert error message and back to prior page
+	{
+		cout << "<script> alert(\"mismatch Password, ConfirmPassword\");" <<endl;
+		cout <<" history.back(); <</script>";
+	}
 
-	Check_Element(f_Username,string("Username"));
-	Check_Element(f_Email,string("Email"));
-	Check_Element(f_Password,string("Password"));
-	Check_Element(f_ConfirmPassword,string("ConfirmPassword"));
+ 	username = trim_space(**f_Username);
+ 	email = trim_space(**f_Email);
+  password = trim_space(**f_Password);
+
+ try{
+   sql::Driver *driver;
+   sql::Connection *con;
+   sql::Statement *stmt;
+   sql::ResultSet *res;
+   sql::PreparedStatement *pstmt;
+
+   driver = get_driver_instance();
+   con = dirver->connect("localhost","root","root");
+   con->setSchema("HTML_DB");
+   string sql ="INSERT INTO user_auth(username,password) VALUES (?,SHA2(?))";
+   pstmt->con.prepareStatement(sql);
+   pstmt->setString(1,username);
+   pstmt->setString(2,password);
+   pstmt->executeUpdate();
+
+   string sql ="INSERT INTO user_profile(username,email) VALUES (?,?)";
+   pstmt->con.prepareStatement(sql);
+   pstmt->setString(1,username);
+   pstmt->setString(2,email);
+   pstmt->executeUpdate();
+   delete pstmt;
+
+ }catch (sql::SQLException &e) {
+  cout << "# ERR: SQLException in " << __FILE__;
+  cout << "(" << __FUNCTION__ << ") on line " »
+     << __LINE__ << endl;
+  cout << "# ERR: " << e.what();
+  cout << " (MySQL error code: " << e.getErrorCode();
+  cout << ", SQLState: " << e.getSQLState() << »
+     " )" << endl;
+}
+//success registration
+ cout << "<script> alert(\"Success Registration\");"<<endl;
+ cout << "location.href = \"localhost/index.html\"</script>"<<endl;
 
 	cout << "<br>\n";
 	cout <<"</body>\n";
@@ -59,8 +105,11 @@ void Check_Element(form_iterator &f,string name) // print parameter's value
 }
 
 
-void trim_space(string &temp)
+
+string trim_space(char* temp_c)
 {
+   string temp = string(temp_c);
+
    int len,end;
    int start_end, end_start, i;
    len = temp.length();
@@ -81,4 +130,6 @@ void trim_space(string &temp)
    end_start = i + 1;
    if (end_start < len)
    temp.replace(end_start, end - end_start, "");
+   return temp;
+
 }
