5f8f6ada36bc009c80ff4daf15be60621ac08daf
PecentZ <security428@naver.com>
2021-05-01 17:22:29 +0900

    testing trim function

--- html/cgi/cgi-bin/register.cpp
+++ html/cgi/cgi-bin/register.cpp
@@ -17,6 +17,7 @@ using namespace cgicc;
 
 Cgicc formData;
 void Check_Element(form_iterator &f,string name); // print parameter's value
+void trim_space(string &temp) // trim  front , back  space
 
 int main()
 {
@@ -31,6 +32,11 @@ int main()
 	form_iterator f_Email = formData.getElement("Email");
 	form_iterator f_Password = formData.getElement("Password");
 	form_iterator f_ConfirmPassword = formData.getElement("ConfirmPassword");
+
+  trim_space(f_Username);
+  trim_space(f_Email);
+  trim_space(f_Password);
+  trim_space(f_ConfirmPassword);
 
 	Check_Element(f_Username,string("Username"));
 	Check_Element(f_Email,string("Email"));
@@ -50,4 +56,29 @@ void Check_Element(form_iterator &f,string name) // print parameter's value
 	}else{
 	cout <<"<p> NO "<<name <<" entered</p>" <<endl;
 	}
+}
+
+
+void trim_space(string &temp)
+{
+   int len,end;
+   int start_end, end_start, i;
+   len = temp.length();
+   for (i = 0; i<len; i++)
+   {
+       if (temp[i] != ' ') break;    // i-1
+   }
+   start_end = i - 1;
+   if (start_end >= 0)
+       temp.replace(0, start_end + 1, "");
+
+   len = temp.length();
+   end = len;
+   for (i = len - 1; i >= 0; i--)
+   {
+       if (temp[i] != ' ') break;  // i+1
+   }
+   end_start = i + 1;
+   if (end_start < len)
+   temp.replace(end_start, end - end_start, "");
 }
