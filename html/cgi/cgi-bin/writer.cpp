cf709fe261dd5d8e2ac3117dcc0c47f59d760b1b
PecentZ <security428@naver.com>
2021-05-02 23:20:04 +0900

    work writer.cpp

--- html/cgi/cgi-bin/writer.cpp
+++ html/cgi/cgi-bin/writer.cpp
@@ -4,16 +4,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <cgicc/cgiDefs.h>
+#include <cgicc/CgiDefs.h>  //for cgic programming
 #include <cgicc/Cgicc.h>
-#include <cgicc/HTTPHTMLHeaer.h>
+#include <cgicc/HTTPHTMLHeader.h>
 #include <cgicc/HTMLClasses.h>
 
+#include "mysql_connection.h"
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+#include <cppconn/prepared_statement.h>
 using namespace std;
 using namespace cgicc;
 
+string trim_space(string);
+
+
 int main() {
 	Cgicc formData;
+	string author_id, content_title,content_text,content_img;
+	string path ="uploads/";
 	cout << "Content-type :text/html\r\n\r\n";
 	cout <<"<html>\n";
 	cout <<"<head>\n";
@@ -28,6 +39,87 @@ int main() {
         trim_dat
 
 
+	sql::Driver *driver;
+  sql::Connection *con;
+	sql::PreparedStatement *pstmt;
+  driver = get_driver_instance();
+  con = driver->connect("localhost","root","root");
+  con->setSchema("HTML_DB");
 
 
- 
+  content_title = trim_space(**f_title);
+	content_text  = **f_description;
+	content_img = make_filename(con,f_file->getFilename());
+
+
+	string sql ="INSERT INTO post_content (content_title,content_text,content_img,date_created,time_written) VALUES (?,?,?,CURDATE(),CURTIME())";
+	pstmt= con->prepareStatement(sql);
+	pstmt->setString(1,path+content_title);
+	pstmt->setString(2,content_text);
+	pstmt->setString(2,content_img);
+	pstmt->executeUpdate();
+
+	delete pstmt;
+	delete con;
+
+	cout << "<script> alert(\"success\");" << endl;
+	cout << "location.href=\"/index.html\" </script>";
+
+ return 0;
+
+
+}
+
+make_filename (sql::Connection &con,string temp_filename)  // check If there is same file name
+{
+
+	string filename=temp_filename;
+	sql::ResultSet *res;
+	sql::PreparedStatement *pstmt;
+
+
+	while(1){
+	string sql ="SELECT * from post_content where content_img = ?";
+  pstmt= con->prepareStatement(sql);
+  pstmt->setString(1,filename);
+  res = pstmt->executeQuery();
+  if(!(res->next())) //  file name is unique
+	break;
+	srand(time_NULL);
+	filename += (rand()%10);
+}
+
+delete pstmt;
+delete res;
+return filename;
+}
+
+
+
+
+string trim_space(string temp)
+{
+
+   int len,end;
+   int start_end, end_start, i;
+   len = temp.length();
+   for (i = 0; i<len; i++)
+   {
+       if (temp[i] != ' ') break;    // i-1
+   }
+   start_end = i - 1;
+   if (start_end >= 0)
+       temp.replace(0, start_end + 1, "");
+
+   len = temp.length();
+   end = len;
+   for (i = len - 1; i >= 0; i--)
+   {
+       if (temp[i] != ' ') break;  // i+1
+   }
+   end_start = i + 1;
+   if (end_start < len)
+   temp.replace(end_start, end - end_start, "");
+   return temp;
+
+}
