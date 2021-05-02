4bdbbf9e5f3d2076ff5e512792a42675e01702b9
PecentZ <security428@naver.com>
2021-05-03 00:15:38 +0900

    deguggin writer.cpp

--- html/cgi/cgi-bin/writer.cpp
+++ html/cgi/cgi-bin/writer.cpp
@@ -19,13 +19,16 @@
 using namespace std;
 using namespace cgicc;
 
+
+bool Check_Element(form_iterator &f);
 string trim_space(string);
 string make_filename (sql::Connection *con,string temp_filename);
+void Insert_DB(form_iterator &, form_iterator &, const_file_iterator &);
 
+
+Cgicc formData;
 int main() {
-	Cgicc formData;
 	string author_id, content_title,content_text,content_img;
-	string path ="uploads/";
 	cout << "Content-type :text/html\r\n\r\n";
 	cout <<"<html>\n";
 	cout <<"<head>\n";
@@ -37,65 +40,34 @@ int main() {
 	form_iterator f_description = formData.getElement("description"); //get description element
 	const_file_iterator f_file = formData.getFile("myfile"); // get myfile element
 
+	if (Check_Element(f_title) &&  Check_Element(f_description) && f_file->getFilename()) // exist
+{
+	Insert_DB(f_title,f_description,f_file);
+	cout << "<script> alert(\"Success\");" << endl;
+	cout << "location.href=\"/index.html\" </script>"<<endl;
+}
+else
+{
+	cout << "<script> alert(\"Fail\");" << endl;
+	cout << "location.href=\"/index.html\" </script>"<<endl;
 
-	sql::Driver *driver;
-  sql::Connection *con;
-	sql::PreparedStatement *pstmt;
-  driver = get_driver_instance();
-  con = driver->connect("localhost","root","root");
-  con->setSchema("HTML_DB");
+}
 
 
-  content_title = trim_space(**f_title);
-	content_text  = **f_description;
-	content_img = make_filename(con,f_file->getFilename());
+cout << "<br>\n";
+cout <<"</body>\n";
+cout <<"</html>\n";
 
-
-	string sql ="INSERT INTO post_content (content_title,content_text,content_img,date_created,time_written) VALUES (?,?,?,CURDATE(),CURTIME())";
-	pstmt= con->prepareStatement(sql);
-	pstmt->setString(1,path+content_title);
-	pstmt->setString(2,content_text);
-	pstmt->setString(2,content_img);
-	pstmt->executeUpdate();
-
-	delete pstmt;
-	delete con;
-
-	cout << "<script> alert(\"success\");" << endl;
-	cout << "location.href=\"/index.html\" </script>";
-	cout << "<br>\n";
-	cout <<"</body>\n";
-	cout <<"</html>\n";
  return 0;
 
 
 }
 
-string make_filename (sql::Connection *con,string temp_filename)  // check If there is same file name
-{
-
-	string filename=temp_filename;
-	sql::ResultSet *res;
-	sql::PreparedStatement *pstmt;
-
-
-	while(1){
-	string sql ="SELECT * from post_content where content_img = ?";
-  pstmt= con->prepareStatement(sql);
-  pstmt->setString(1,filename);
-  res = pstmt->executeQuery();
-  if(!(res->next())) //  file name is unique
-	break;
-	srand(time(NULL));
-	filename += (rand()%10);
+bool Check_Element(form_iterator &f) // print parameter's value
+{ //also needed check space or valid value
+	if(!f->isEmpty() && f != (*formData).end()) return true;
+	else return false;
 }
-
-delete pstmt;
-delete res;
-return filename;
-}
-
-
 
 
 string trim_space(string temp)
@@ -122,5 +94,63 @@ string trim_space(string temp)
    if (end_start < len)
    temp.replace(end_start, end - end_start, "");
    return temp;
+
+}
+
+
+string make_filename (sql::Connection *con,string temp_filename)  // check If there is same file name
+{
+
+	string filename=temp_filename;
+	sql::ResultSet *res;
+	sql::PreparedStatement *pstmt;
+
+	while(1){
+	string sql ="SELECT * from post_content where content_img = ?";
+  pstmt= con->prepareStatement(sql);
+  pstmt->setString(1,filename);
+  res = pstmt->executeQuery();
+  if(!(res->next())) //  file name is unique
+	break;
+	srand(time(NULL));
+	filename += (rand()%10);
+}
+
+delete pstmt;
+delete res;
+return filename;
+}
+
+
+
+
+
+void Insert_DB(form_iterator &f_title , form_iterator &f_description , const_file_iterator &f_file)
+{
+	string content_title, content_text,content_img;
+	string path ="uploads/";
+
+	sql::Driver *driver;
+	sql::Connection *con;
+	sql::PreparedStatement *pstmt;
+	driver = get_driver_instance();
+	con = driver->connect("localhost","root","root");
+	con->setSchema("HTML_DB");
+
+
+	content_title = trim_space(**f_title);
+	content_text  = **f_description;
+	content_img = make_filename(con,f_file->getFilename());
+
+
+	string sql ="INSERT INTO post_content (content_title,content_text,content_img,date_created,time_written) VALUES (?,?,?,CURDATE(),CURTIME())";
+	pstmt= con->prepareStatement(sql);
+	pstmt->setString(1,path+content_title);
+	pstmt->setString(2,content_text);
+	pstmt->setString(2,content_img);
+	pstmt->executeUpdate();
+
+	delete pstmt;
+	delete con;
 
 }
