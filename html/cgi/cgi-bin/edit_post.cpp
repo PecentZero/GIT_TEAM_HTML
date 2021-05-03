052ec9ba940b229592d533282b88e182fcbfcb31
PecentZ <security428@naver.com>
2021-05-04 00:47:01 +0900

    rename filename

--- html/cgi/cgi-bin/edit_post.cpp
+++ html/cgi/cgi-bin/edit_post.cpp
@@ -1,76 +1,67 @@
-#include <iostream>
 
 
-using namespace std;
 
-int main()
-{
-
-cout <<"Content-type:text/html\r\n\r\n";
-cout << "<html>";
-cout << "";
-cout << "<head>";
-cout << "";
-cout << "<link href=\"../../vendor/bootstrap/css/bootstrap.min.css\" rel=\"stylesheet\">";
-cout << "<link href=\"../../css/style_post_create.css\" rel=\"stylesheet\">";
-cout << " <link rel=\"stylesheet\" href=\"../../css/bootstrap.css\">";
-cout << "        <link rel=\"stylesheet\" href=\"../../vendors/linericon/style.css\">";
-cout << "        <link rel=\"stylesheet\" href=\"../../css/font-awesome.min.css\">";
-cout << "        <link rel=\"stylesheet\" href=\"../../vendors/lightbox/simpleLightbox.css\">";
-cout << "        <link rel=\"stylesheet\" href=\"../../vendors/nice-select/css/nice-select.css\">";
-cout << "        <!-- main css -->";
-cout << "        <link rel=\"stylesheet\" href=\"../../css/style.css\">";
-cout << "        <link rel=\"stylesheet\" href=\"../../css/responsive.css\">";
-cout << "</head>";
-cout << "<body>";
-cout << "<div class=\"container\">";
-cout << "	<div class=\"row\">";
-cout << "	    ";
-cout << "	    <div class=\"col-md-8 col-md-offset-2\">";
-cout << "	        ";
-cout << "    		<h1>Create post</h1>";
-cout << "    		";
-cout << "    		<form enctype =\"multipart/form-data\" action=\"/writer.cgi\" method=\"POST\">";
-cout << "    		    ";
-cout << "    		    ";
-cout << "    		    ";
-cout << "    		    <div class=\"form-group\">";
-cout << "    		        <label for=\"title\">Title <span class=\"require\">*</span></label>";
-cout << "    		        <input type=\"text\" class=\"form-control\" name=\"title\" />";
-cout << "    		    </div>";
-cout << "    		    ";
-cout << "    		    <div class=\"form-group\">";
-cout << "    		        <label for=\"description\">Description</label>";
-cout << "    		        <textarea rows=\"5\" class=\"form-control\" name=\"description\" ></textarea>";
-cout << "    		    </div>";
-cout << "    		    ";
-cout << "    		    ";
-cout << "    		    <div>";
-cout << "    		    	<input type=\"file\" id=\"myfile\" name=\"myfile\"/><br><br>";
-cout << "    		    </div>";
-cout << "    		    ";
-cout << "    		    <div class=\"form-group\">";
-cout << "    		        <button type=\"submit\" class=\"btn btn-primary\">";
-cout << "    		            Create";
-cout << "    		        </button>";
-cout << "    		        <button class=\"btn btn-default\">";
-cout << "    		            Cancel";
-cout << "    		        </button>";
-cout << "    		    </div>";
-cout << "    		    ";
-cout << "    		</form>";
-cout << "		</div>";
-cout << "		";
-cout << "	</div>";
-cout << "</div>";
-cout << "";
-cout << "</body>";
-cout << "";
-cout << "<!-- <script src=\"//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js\"></script>";
-cout << "<script src=\"//code.jquery.com/jquery-1.11.1.min.js\"></script> -->";
-cout << "</html>";
-cout << "";
-
-return 0;
-
-}
+cout << "Content-type:text/html\r\n\r\n";
+cout << "<html>\n";
+cout << "\n";
+cout << "<head>\n";
+cout << "\n";
+cout << "<link href=\"vendor/bootstrap/css/bootstrap.min.css\" rel=\"stylesheet\">\n";
+cout << "<link href=\"css/style_post_create.css\" rel=\"stylesheet\">\n";
+cout << " <link rel=\"stylesheet\" href=\"css/bootstrap.css\">\n";
+cout << "        <link rel=\"stylesheet\" href=\"vendors/linericon/style.css\">\n";
+cout << "        <link rel=\"stylesheet\" href=\"css/font-awesome.min.css\">\n";
+cout << "        <link rel=\"stylesheet\" href=\"vendors/lightbox/simpleLightbox.css\">\n";
+cout << "        <link rel=\"stylesheet\" href=\"vendors/nice-select/css/nice-select.css\">\n";
+cout << "        <!-- main css -->\n";
+cout << "        <link rel=\"stylesheet\" href=\"css/style.css\">\n";
+cout << "        <link rel=\"stylesheet\" href=\"css/responsive.css\">\n";
+cout << "</head>\n";
+cout << "<body>\n";
+cout << "<div class=\"container\">\n";
+cout << "	<div class=\"row\">\n";
+cout << "	    \n";
+cout << "	    <div class=\"col-md-8 col-md-offset-2\">\n";
+cout << "	        \n";
+cout << "    		<h1>Create post</h1>\n";
+cout << "    		\n";
+cout << "    		<form enctype =\"multipart/form-data\" action=\"/cgi-bin/writer.cgi\" method=\"POST\">\n";
+cout << "    		    \n";
+cout << "    		    \n";
+cout << "    		    \n";
+cout << "    		    <div class=\"form-group\">\n";
+cout << "    		        <label for=\"title\">Title <span class=\"require\">*</span></label>\n";
+cout << "    		        <input type=\"text\" class=\"form-control\" name=\"title\" />\n";
+cout << "    		    </div>\n";
+cout << "    		    \n";
+cout << "    		    <div class=\"form-group\">\n";
+cout << "    		        <label for=\"description\">Description</label>\n";
+cout << "    		        <textarea rows=\"5\" class=\"form-control\" name=\"description\" ></textarea>\n";
+cout << "    		    </div>\n";
+cout << "    		    \n";
+cout << "    		    \n";
+cout << "    		    <div>\n";
+cout << "    		    	<input type=\"file\" id=\"myfile\" name=\"myfile\"/><br><br>\n";
+cout << "    		    </div>\n";
+cout << "    		    \n";
+cout << "    		    <div class=\"form-group\">\n";
+cout << "    		        <button type=\"submit\" class=\"btn btn-primary\">\n";
+cout << "    		            Create\n";
+cout << "    		        </button>\n";
+cout << "    		        <button class=\"btn btn-default\">\n";
+cout << "    		            Cancel\n";
+cout << "    		        </button>\n";
+cout << "    		    </div>\n";
+cout << "    		    \n";
+cout << "    		</form>\n";
+cout << "		</div>\n";
+cout << "		\n";
+cout << "	</div>\n";
+cout << "</div>\n";
+cout << "\n";
+cout << "</body>\n";
+cout << "\n";
+cout << "<!-- <script src=\"//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js\"></script>\n";
+cout << "<script src=\"//code.jquery.com/jquery-1.11.1.min.js\"></script> -->\n";
+cout << "</html>\n";
+cout << "\n";
