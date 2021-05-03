2d8fbd4414f08e86793b14829e4375468f52d424
PecentZ <security428@naver.com>
2021-05-03 21:18:31 +0900

    start edit_post.cgi

---
+++ html/cgi/cgi-bin/edit.post.cpp
@@ -0,0 +1,66 @@
+Content-type:text/html
+
+
+cout << "<html>";
+cout << "";
+cout << "<head>";
+cout << "";
+cout << "<link href=\"vendor/bootstrap/css/bootstrap.min.css\" rel=\"stylesheet\">";
+cout << "<link href=\"css/style_post_create.css\" rel=\"stylesheet\">";
+cout << " <link rel=\"stylesheet\" href=\"css/bootstrap.css\">";
+cout << "        <link rel=\"stylesheet\" href=\"vendors/linericon/style.css\">";
+cout << "        <link rel=\"stylesheet\" href=\"css/font-awesome.min.css\">";
+cout << "        <link rel=\"stylesheet\" href=\"vendors/lightbox/simpleLightbox.css\">";
+cout << "        <link rel=\"stylesheet\" href=\"vendors/nice-select/css/nice-select.css\">";
+cout << "        <!-- main css -->";
+cout << "        <link rel=\"stylesheet\" href=\"css/style.css\">";
+cout << "        <link rel=\"stylesheet\" href=\"css/responsive.css\">";
+cout << "</head>";
+cout << "<body>";
+cout << "<div class=\"container\">";
+cout << "	<div class=\"row\">";
+cout << "	    ";
+cout << "	    <div class=\"col-md-8 col-md-offset-2\">";
+cout << "	        ";
+cout << "    		<h1>Create post</h1>";
+cout << "    		";
+cout << "    		<form enctype =\"multipart/form-data\" action=\"/cgi-bin/writer.cgi\" method=\"POST\">";
+cout << "    		    ";
+cout << "    		    ";
+cout << "    		    ";
+cout << "    		    <div class=\"form-group\">";
+cout << "    		        <label for=\"title\">Title <span class=\"require\">*</span></label>";
+cout << "    		        <input type=\"text\" class=\"form-control\" name=\"title\" />";
+cout << "    		    </div>";
+cout << "    		    ";
+cout << "    		    <div class=\"form-group\">";
+cout << "    		        <label for=\"description\">Description</label>";
+cout << "    		        <textarea rows=\"5\" class=\"form-control\" name=\"description\" ></textarea>";
+cout << "    		    </div>";
+cout << "    		    ";
+cout << "    		    ";
+cout << "    		    <div>";
+cout << "    		    	<input type=\"file\" id=\"myfile\" name=\"myfile\"/><br><br>";
+cout << "    		    </div>";
+cout << "    		    ";
+cout << "    		    <div class=\"form-group\">";
+cout << "    		        <button type=\"submit\" class=\"btn btn-primary\">";
+cout << "    		            Create";
+cout << "    		        </button>";
+cout << "    		        <button class=\"btn btn-default\">";
+cout << "    		            Cancel";
+cout << "    		        </button>";
+cout << "    		    </div>";
+cout << "    		    ";
+cout << "    		</form>";
+cout << "		</div>";
+cout << "		";
+cout << "	</div>";
+cout << "</div>";
+cout << "";
+cout << "</body>";
+cout << "";
+cout << "<!-- <script src=\"//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js\"></script>";
+cout << "<script src=\"//code.jquery.com/jquery-1.11.1.min.js\"></script> -->";
+cout << "</html>";
+cout << "";
