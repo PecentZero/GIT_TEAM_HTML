16b0c9c1a5b3997bec4c2c094291bf8b27afb506
PecentZero <52845759+PecentZero@users.noreply.github.com>
2021-05-07 23:42:46 +0900

    Update build.sh

--- cgi-bin/build.sh
+++ cgi-bin/build.sh
@@ -2,14 +2,15 @@
 
 cd /var/www/cgi-bin/
 
-rm *.cgi
-g++ -o login.cgi login.cpp cookie.cpp -lmysqlcppconn -lcgicc
-g++ -o logout.cgi logout.cpp -lmysqlcppconn -lcgicc
-g++ -o create_post.cgi create_post.cpp -lmysqlcppconn -lcgicc
-g++ -o index.cgi index.cpp cookie.cpp -lmysqlcppconn -lcgicc
-g++ -o profile.cgi profile.cpp cookie.cpp -lmysqlcppconn -lcgicc
-g++ -o register.cgi register.cpp -lmysqlcppconn -lcgicc
-g++ -o edit_post.cgi edit_post.cpp -lmysqlcppconn -lcgicc
-g++ -o delete_post.cgi delete_post.cpp -lmysqlcppconn -lcgicc
-g++ -o settings.cgi settings.cpp cookie.cpp -lmysqlcppconn -lcgicc
-g++ -o create_adv.cgi create_adv.cpp -lmysqlcppconn -lcgicc
+rm /var/www/cgi-bin/*.cgi
+g++ -o login.cgi login.cpp cookie.cpp -lmysqlcppconn -lcgicc -z execstack -fno-stack-protector -z norelro -g -O0
+g++ -o logout.cgi logout.cpp -lmysqlcppconn -lcgicc -z execstack -fno-stack-protector -z norelro -g -O0
+g++ -o create_post.cgi create_post.cpp -lmysqlcppconn -lcgicc -z execstack -fno-stack-protector -z norelro -g -O0
+g++ -o index.cgi index.cpp cookie.cpp -lmysqlcppconn -lcgicc -z execstack -fno-stack-protector -z norelro -g -O0
+g++ -o profile.cgi profile.cpp cookie.cpp -lmysqlcppconn -lcgicc -z execstack -fno-stack-protector -z norelro -g -O0
+g++ -o register.cgi register.cpp -lmysqlcppconn -lcgicc -z execstack -fno-stack-protector -z norelro -g -O0
+g++ -o edit_post.cgi edit_post.cpp -lmysqlcppconn -lcgicc -z execstack -fno-stack-protector -z norelro -g -O0
+g++ -o delete_post.cgi delete_post.cpp -lmysqlcppconn -lcgicc -z execstack -fno-stack-protector -z norelro -g -O0
+g++ -o settings.cgi settings.cpp cookie.cpp -lmysqlcppconn -lcgicc -z execstack -fno-stack-protector -z norelro -g -O0
+g++ -o create_adv.cgi create_adv.cpp -lmysqlcppconn -lcgicc -z execstack -fno-stack-protector -z norelro -g -O0
+ 
